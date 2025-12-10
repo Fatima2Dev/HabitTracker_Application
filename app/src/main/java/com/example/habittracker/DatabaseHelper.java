ba303d08902273ea0a0f17fd7457f056b7e87be5
roaatur <roaatura@gmail.com>
2025-12-10 08:43:01 +0300

    database

---
+++ app/src/main/java/com/example/habittracker/DatabaseHelper.java
@@ -0,0 +1,54 @@
+package com.example.habittracker;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "HabitTracker.db";
+    public static final String TABLE_NAME = "users";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "EMAIL";
+    public static final String COL_3 = "PASSWORD";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, EMAIL TEXT, PASSWORD TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    // Method to add a new user (Sign Up)
+    public boolean addUser(String email, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, email);
+        contentValues.put(COL_3, password);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1; // returns true if insert is successful
+    }
+
+    // Method to check if user exists (Log In)
+    public boolean checkUser(String email, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = { COL_1 };
+        String selection = COL_2 + "=? AND " + COL_3 + "=?";
+        String[] selectionArgs = { email, password };
+
+        Cursor cursor = db.query(TABLE_NAME, columns, selection, selectionArgs, null, null, null);
+        int count = cursor.getCount();
+        cursor.close();
+        return count > 0; // returns true if user exists
+    }
+}
